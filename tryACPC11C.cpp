5ff05208250d63eeb4fb6d7b178f2cb1c5704f3d
Ravi Kishore <ravikishore1993@gmail.com>
2012-02-01 20:31:10 +0530

    Just indented the code.

--- tryACPC11C.cpp
+++ tryACPC11C.cpp
@@ -1,21 +1,28 @@
 #include<stdio.h>
-#include<algorithm>
+#include<algorithm>                        //Used a STL . must recheck why i had used this header.
 main()
-{long long int sum,q,a,j;
-scanf("%lld",&q);
-while(q!=0){
-scanf("%lld",&a);
-long long int l[a];
-for(j=0;j<a;j++)
-scanf("%lld",&l[j]);
-if(l[0]>l[a-1])
-{sum=0;
-               for(j=1;j<a;j++)
-sum=sum+l[j];}
-else
-{sum=0;
-    for(j=0;j<a-1;j++)
-sum=sum+l[j];}
-printf("%lld\n",sum);
-q--;}
-      }
+{
+	long long int sum,q,a,j;
+	scanf("%lld",&q);
+	while(q!=0)
+	{
+		scanf("%lld",&a);
+		long long int l[a];
+		for(j=0;j<a;j++)
+		scanf("%lld",&l[j]);
+		if(l[0]>l[a-1])
+		{ 	
+			sum=0;
+               		for(j=1;j<a;j++)
+			sum=sum+l[j];
+		}
+			else
+		{
+			sum=0;
+   			for(j=0;j<a-1;j++)
+			sum=sum+l[j];
+		}
+		printf("%lld\n",sum);
+		q--;
+	}
+}

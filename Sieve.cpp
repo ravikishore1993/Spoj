ac9c52b9453a9cb92170fdec57ee310f06fd2d5d
Sukeesh <vsukeeshbabu@gmail.com>
2015-10-27 19:12:41 +0530

    Update Sieve.cpp
    Faster than previous one :p .
    Previous Sieve doesn't work for http://www.spoj.com/problems/DIVFACT/

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,23 +1,27 @@
 #include<stdio.h>
 #include<iostream>
 #include<math.h>
+#include<map>
 using namespace std;
+map<int,int> a;
+const int MAX = 510000;
 main()
 {
-      int a[510000]={0};
-      for(int t=1;t<510000;t=t+2)
-      a[t]=1;
-      for(int i=3;i<=sqrt(510000);i=i+2)
+      for(int i=3;i<sqrt(MAX+1);i+=2)
       {
-              for(int j=3;i*j<=510000;j=j+2)
-              {a[i*j]=0;
-                      }
-              }
-      for(int j=1;j<510000;j++)
-      { if(a[j]==1)
-       printf("%d\n",j);}
-      // system("pause");
-
-
+        if(a[i/2]==0)
+        {
+            for(j=i*i;j<MAX+1;j+=2*i)
+            {
+                a[j/2]=1;
+            }
+        }
       }
+      for(int j=1;j<510000;j++)
+      {     
+            if(a[j]==0){
+                  printf("%d\n",j);
+            }
+      }
+}
 
